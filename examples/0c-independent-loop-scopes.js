b937c4573f205177e0a9684855cd923a99ed7334
Salman H <Salman0112358@outlook.com>
2022-07-04 20:58:33 +0100

    identifiying limited scope variables in loop

--- examples/0c-independent-loop-scopes.js
+++ examples/0c-independent-loop-scopes.js
@@ -6,13 +6,13 @@ const healthyFoods = ['fruit', 'veg'];
 const junkFoods = ['chocolate', 'cake', 'poison'];
 
 for (let healthyItem of healthyFoods) {
-  console.log(`${healthyItem} is normally considered a healthy food`);
+  console.log(`${healthyItem} is normally considered a healthy food`); // will show 
 }
 
 for (let junkItem of junkFoods) {
-  console.log(`${junkItem} is NOT normally considered a healthy food`);
+  console.log(`${junkItem} is NOT normally considered a healthy food`); // will show 
   // below will error. Why?
-  console.log(`...so, swap it for ${healthyItem}!`);
+  //console.log(`...so, swap it for ${healthyItem}!`); // wont show 
 }
 
-console.log("Reached the end of my programme!");
+console.log("Reached the end of my programme!"); // will show 
